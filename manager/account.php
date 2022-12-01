b74db1bc00943ed517697170a1a95dd7ccc7b286
aimedidierm <aimedidiermugisha@gmail.com>
2022-12-01 14:30:19 +0200

    Anthentication

--- manager/account.php
+++ manager/account.php
@@ -4,4 +4,121 @@ ini_set('display_startup_errors',1);
 error_reporting(E_ALL);
 require '../php-includes/connect.php';
 require 'php-includes/check-login.php';
+$query = "SELECT * FROM manager WHERE email= ? limit 1";
+$stmt = $db->prepare($query);
+$stmt->execute(array($_SESSION['email']));
+$rows = $stmt->fetch(PDO::FETCH_ASSOC);
+if ($stmt->rowCount()>0) {
+    $names=$rows['names'];
+    $email=$rows['email'];
+    $address=$rows['address'];
+    $phone=$rows['phone'];
+}
+if(isset($_POST['update'])){
+$uaddress=$_POST['address'];
+$uphone=$_POST['phone'];
+$cpassword=md5($_POST['cpassword']);
+$apassword=md5($_POST['apassword']);
+if ($apassword == $cpassword){
+    if($apassword == $cpassword){
+        $sql ="UPDATE manager SET address = ?, phone = ? , password = ? WHERE email = ? limit 1";
+        $stm = $db->prepare($sql);
+        if ($stm->execute(array($uaddress, $uphone, $cpassword, $_SESSION['email']))) {
+            print "<script>alert('your data updated');window.location.assign('account.php')</script>";
+
+            }
+    } else{
+        echo "<script>alert('Passwords are not match');window.location.assign('account.php')</script>";
+    }
+} else{
+    echo "<script>alert('Passwords are not match');window.location.assign('account.php')</script>";
+}
+}
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Smart menu information and ordering system</title>
+  <!-- plugins:css -->
+  <link rel="stylesheet" href="../vendors/mdi/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="../vendors/base/vendor.bundle.base.css">
+  <!-- endinject -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="../css/style.css">
+  <!-- endinject -->
+  <link rel="shortcut icon" href="../images/favicon.png" />
+</head>
+
+<body>
+  <div class="container-scroller">
+    <!-- partial:../partials/_navbar.html -->
+    <?php require 'php-includes/nav.php';?>
+      <!-- partial -->
+      <div class="main-panel">
+        <div class="content-wrapper">
+          <div class="row">
+          <div class="col-md-12 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                  <h4 class="card-title">Update your data</h4>
+                  <form class="forms-sample" method="POST">
+                  <div class="form-group input-group">
+                <span class="input-group-addon" style="width:150px;">Names:</span>
+                <input type="text" style="width:350px;" class="form-control" name="names" value="<?php echo $names;?>" disabled>
+                </div>
+                <div class="form-group input-group">
+                    <span class="input-group-addon" style="width:150px;">Email:</span>
+                    <input type="text" style="width:350px;" class="form-control" name="email" value="<?php echo $email;?>" disabled>
+                </div>
+                <div class="form-group input-group">
+                    <span class="input-group-addon" style="width:150px;">Phone:</span>
+                    <input type="text" style="width:350px;" class="form-control" name="phone" value="<?php echo $phone; ?>">
+                </div>
+                <div class="form-group input-group">
+                    <span class="input-group-addon" style="width:150px;">Address:</span>
+                    <input type="text" style="width:350px;" class="form-control" name="address" value="<?php echo $address; ?>">
+                </div>
+                <div class="form-group input-group">
+                    <span class="input-group-addon" style="width:150px;">Password:</span>
+                    <input type="password" style="width:350px;" class="form-control" name="apassword" required>
+                </div>
+                <div class="form-group input-group">
+                    <span class="input-group-addon" style="width:150px;">Confirm password:</span>
+                    <input type="password" style="width:350px;" class="form-control" name="cpassword" required>
+                </div>
+                    <button type="submit" name="update" class="btn btn-primary me-2">Update</button>
+                    <button class="btn btn-light">Cancel</button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- content-wrapper ends -->
+        <!-- partial:../partials/_footer.html -->
+        <?php require 'php-includes/footer.php';?>
+        <!-- partial -->
+      </div>
+      <!-- main-panel ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+  </div>
+  <!-- container-scroller -->
+  <!-- plugins:js -->
+  <script src="../vendors/base/vendor.bundle.base.js"></script>
+  <!-- endinject -->
+  <!-- Plugin js for this page-->
+  <!-- End plugin js for this page-->
+  <!-- inject:js -->
+  <script src="../js/off-canvas.js"></script>
+  <script src="../js/hoverable-collapse.js"></script>
+  <script src="../js/template.js"></script>
+  <!-- endinject -->
+  <!-- Custom js for this page-->
+  <!-- End custom js for this page-->
+</body>
+</html>
---
+++ manager/php-includes/nav.php
@@ -0,0 +1,77 @@
+<?php
+$sql = "SELECT *  FROM manager WHERE email = ? limit 1";
+$stmt = $db->prepare($sql);
+$stmt->execute(array($_SESSION['email']));
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+$names=$row['names'];
+?>
+<nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+      <div class="navbar-brand-wrapper d-flex justify-content-center">
+        <div class="navbar-brand-inner-wrapper d-flex justify-content-between align-items-center w-100">  
+          <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
+            <span class="mdi mdi-sort-variant"></span>
+          </button>
+        </div>  
+      </div>
+      <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
+        <ul class="navbar-nav navbar-nav-right">
+          <li class="nav-item nav-profile dropdown">
+            <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" id="profileDropdown">
+              <img src="../images/faces/face.jpg" alt="profile"/>
+              <span class="nav-profile-name"><?php echo $names?></span>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
+              <a class="dropdown-item" href="account.php">
+                <i class="mdi mdi-settings text-primary"></i>
+                Settings
+              </a>
+              <a class="dropdown-item" href="../php-includes/logout.php">
+                <i class="mdi mdi-logout text-primary"></i>
+                Logout
+              </a>
+            </div>
+          </li>
+        </ul>
+        <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
+          <span class="mdi mdi-menu"></span>
+        </button>
+      </div>
+    </nav>
+    <!-- partial -->
+    <div class="container-fluid page-body-wrapper">
+      <!-- partial:../partials/_sidebar.html -->
+      <nav class="sidebar sidebar-offcanvas" id="sidebar">
+        <ul class="nav">
+          <li class="nav-item">
+            <a class="nav-link" href="orders.php">
+              <i class="mdi mdi-grid-large menu-icon"></i>
+              <span class="menu-title">Orders</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
+              <i class="mdi mdi-account menu-icon"></i>
+              <span class="menu-title">User management</span>
+              <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="auth">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="waiter.php"> Waiter</a></li>
+                <li class="nav-item"> <a class="nav-link" href="chiefs.php"> Chiefs</a></li>
+              </ul>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="account.php">
+              <i class="mdi mdi-settings menu-icon"></i>
+              <span class="menu-title">Profile</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../php-includes/logout.php">
+              <i class="mdi mdi-logout menu-icon"></i>
+              <span class="menu-title">Logout</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
