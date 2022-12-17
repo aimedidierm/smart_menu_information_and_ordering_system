ffd0c9f859d08fde5b6c27e936fcf3c01f80039a
aimedidierm <aimedidiermugisha@gmail.com>
2022-12-18 01:33:26 +0200

    Hardware with sofatware

--- data.php
+++ data.php
@@ -6,9 +6,16 @@ require 'php-includes/connect.php';
 include_once("vendor/autoload.php");
 use Yvesniyo\IntouchSms\SmsSimple;
 /** @var \Yvesniyo\IntouchSms\SmsSimple */
-if(isset($_POST['order'])){
+$query = "SELECT * FROM waiter limit 1";
+$stmt = $db->prepare($query);
+$stmt->execute();
+$rows = $stmt->fetch(PDO::FETCH_ASSOC);
+if ($stmt->rowCount()>0) {
+    $telephone=$rows['phone'];
+}
+if(isset($_GET['order'])){
     //order sample codes here
-    
+    $food=$_GET['order'];
     $sql ="INSERT INTO orders (table_name,orders) VALUES ('Table 1',?)";
     $stm = $db->prepare($sql);
     if ($stm->execute(array($food))) {
@@ -18,7 +25,7 @@ if(isset($_POST['order'])){
             ->message($messi)
             ->sender("+250785063201")
             ->username("kwizerafisto")
-            ->password("kwizera@123")
+            ->password("kwizera123")
             ->apiUrl("www.intouchsms.co.rw/api/sendsms/.json")
             ->callBackUrl("");
         print_r($sms->send());
@@ -28,20 +35,13 @@ if(isset($_POST['order'])){
 
 // Button codes here
 if(isset($_POST['call'])){
-$query = "SELECT * FROM waiter limit 1";
-$stmt = $db->prepare($query);
-$stmt->execute();
-$rows = $stmt->fetch(PDO::FETCH_ASSOC);
-if ($stmt->rowCount()>0) {
-    $telephone=$rows['phone'];
-}
 $messi="Client is calling for you on table 1";
 $sms = new SmsSimple();
 $sms->recipients([$telephone])
     ->message($messi)
     ->sender("+250785063201")
     ->username("kwizerafisto")
-    ->password("kwizera@123")
+    ->password("kwizera123")
     ->apiUrl("www.intouchsms.co.rw/api/sendsms/.json")
     ->callBackUrl("");
 print_r($sms->send());
